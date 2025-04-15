d79531e654e505a7d3316b474d28b3bb7c23b12e
WolfGamer2 <rushilchopra@gmail.com>
2025-04-14 18:46:20 -0700

    new things

--- die.py
+++ die.py
@@ -6,6 +6,7 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("DIE DIE DIE")
+clock = pygame.time.Clock()
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
